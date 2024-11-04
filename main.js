200ed7281976f7f2cff58431f479759e9e38a236
Manuel <105811018+Manux9512@users.noreply.github.com>
2024-11-04 12:48:43 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -18,9 +18,11 @@ window.addEventListener("load", () => {
 
 function scrollToElement(params) {
     document.getElementById("click").scrollIntoView({
-        behavior: "smooth"
+        behavior: "smooth" 
     })
 }
+
+
 
 
 /* ----------------------------------------
@@ -44,9 +46,9 @@ testimonialSlider();
             Validation Form
 ------------------------------------------- */
 if (document.body.classList.contains('contact-page')) {
-    document.getElementById('contactForm').addEventListener('submit', function (event) {
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent the form from submitting the default way
-
+    
         // Validate the input fields
         if (validateForm()) {
             // Send the email if validation passes
@@ -140,8 +142,8 @@ function sendEmail() {
 ------------------------------------------- */
 function headerMenu() {
     const menu = document.querySelector(".js-header-menu"),
-        backdrop = document.querySelector(".js-header-backdrop"),
-        menuCollapseBreakpoint = 991;
+    backdrop = document.querySelector(".js-header-backdrop"),
+    menuCollapseBreakpoint = 991;
 
     function toggleMenu() {
         menu.classList.toggle("open");
@@ -186,7 +188,7 @@ function headerMenu() {
     });
 
     // When resizing window...
-    window.addEventListener("resize", function () {
+    window.addEventListener("resize", function() {
         if (this.innerWidth > menuCollapseBreakpoint && menu.classList.contains("open")) {
             toggleMenu();
         }
@@ -205,9 +207,9 @@ headerMenu();
 ------------------------------------------- */
 function styleSwitcherToggle() {
     const styleSwitcher = document.querySelector(".js-style-switcher"),
-        styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
+    styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
 
-    styleSwitcherToggler.addEventListener("click", function () {
+    styleSwitcherToggler.addEventListener("click", function() {
         styleSwitcher.classList.toggle("open");
         this.querySelector("i").classList.toggle("fa-times");
         this.querySelector("i").classList.toggle("fa-cog");
@@ -224,7 +226,7 @@ styleSwitcherToggle();
 ------------------------------------------- */
 function themeColors() {
     const colorStyle = document.querySelector(".js-color-style"),
-        themeColorsContainer = document.querySelector(".js-theme-colors");
+    themeColorsContainer = document.querySelector(".js-theme-colors");
 
     themeColorsContainer.addEventListener("click", ({ target }) => {
         if (target.classList.contains("js-theme-color-item")) {
@@ -261,7 +263,7 @@ themeColors();
 function themeLightDark() {
     const darkModeCheckbox = document.querySelector(".js-dark-mode");
 
-    darkModeCheckbox.addEventListener("click", function () {
+    darkModeCheckbox.addEventListener("click", function() {
         if (this.checked) {
             localStorage.setItem("theme-dark", "true");
         } else {
@@ -294,9 +296,9 @@ themeLightDark();
 ------------------------------------------- */
 function themeGlassEffect() {
     const glassEffectCheckbox = document.querySelector(".js-glass-effect"),
-        glassStyle = document.querySelector(".js-glass-style");
+    glassStyle = document.querySelector(".js-glass-style");
 
-    glassEffectCheckbox.addEventListener("click", function () {
+    glassEffectCheckbox.addEventListener("click", function() {
         if (this.checked) {
             localStorage.setItem("glass-effect", "true");
         } else {
@@ -323,8 +325,34 @@ function themeGlassEffect() {
 }
 themeGlassEffect();
 
+/* ----------------------------------------
+            Product Registration Form
+------------------------------------------- */
+
+function productRegistrationForm() {
+    const productObject = {
+        name: '',
+        imageUrl: '',
+        description: ''
+        
+    };
+
+    const productForm = document.querySelector("#productRegistrationForm");
+    const name = document.querySelector("#productName");
+    const image = document.querySelector("#productImage");
+    const description = document.querySelector("#productDescription");
+    
+    productForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        productObject.name = name.value;
+        productObject.description = description.value;
+        productObject.imageUrl = image.value;
+        
+        const productObjectJSON = JSON.stringify(productObject);
+        console.log(productObjectJSON);
+    }); 
+}
+productRegistrationForm();
+
 // 45:25_35,100yards_Biotic_SeNt // //  ML_CerbAg_Opt_Perf  //  //  Warp, Overload & Slam
 //
-
-
-
